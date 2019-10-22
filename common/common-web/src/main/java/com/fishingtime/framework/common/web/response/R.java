ad5dc1f6e04e5970b91cb65e609c5619a7b945e5
杨正彬 <yangzb@tydic.com>
2019-10-22 09:41:37 +0800

    新增

--- common/common-web/src/main/java/com/fishingtime/framework/common/web/response/R.java
+++ common/common-web/src/main/java/com/fishingtime/framework/common/web/response/R.java
@@ -14,15 +14,22 @@ import java.util.Optional;
  * @create: 2019-10-18 17:24
  **/
 public class R<T> extends LinkedHashMap<String,Object> implements Serializable {
-    R(){
+    R(ResultStatus status,String message,T data){
+        this.data = data;
+        Optional.ofNullable(data).ifPresent(o -> {
+            this.putAll(BeanUtil.beanToMap(this.data));
+        });
+        this.status = Optional.ofNullable(status).orElse(this.status);
+        this.put(CODE,this.status.getCode());
+        this.message = Optional.ofNullable(message).orElse(this.status.getMessage());
+        this.put(MESSAGE,this.message);
     }
-    public String getCode() {
-        return status.getCode();
-    }
-
     public String getMessage() {
         message = Optional.ofNullable(message).orElse(status.getMessage());
         return message;
+    }
+    public String getCode() {
+        return status.getCode();
     }
 
     public static final String CODE = "code";
@@ -30,32 +37,6 @@ public class R<T> extends LinkedHashMap<String,Object> implements Serializable {
 
     public T getData() {
         return data;
-    }
-
-    public R setStatus(ResultStatus status) {
-        this.status = status;
-        return  this;
-    }
-
-    public R setMessage(String message) {
-        this.message = message;
-        return  this;
-    }
-
-    public R setData(T data) {
-        Optional.ofNullable(data).ifPresent(o -> {
-            this.putAll(BeanUtil.beanToMap(data));
-        });
-        this.data = data;
-        return  this;
-    }
-
-    private void set(ResultStatus status,String message){
-        message = Optional.ofNullable(message).orElse(status.getMessage());
-        status = Optional.ofNullable(status).orElse(status);
-
-        this.put(CODE,status.getCode());
-        this.put(MESSAGE,message);
     }
 
     private ResultStatus status = ResultStatus.SUCCESS;
--- common/common-web/src/main/java/com/fishingtime/framework/common/web/response/Response.java
+++ common/common-web/src/main/java/com/fishingtime/framework/common/web/response/Response.java
@@ -20,20 +20,20 @@ public class Response {
     public static final HttpHeaders HEADERS = new HttpHeaders() {{
         add("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
     }};
-    public static final R DEFAULT_RESPONSE_BODY_OK = new R();
+    public static final R DEFAULT_RESPONSE_BODY_OK = new R(ResultStatus.SUCCESS,null,null);
 
     public static ResponseEntity<R> ok(ResultStatus resultStatus,String message) {
-        return result(null,null,new R().setStatus(resultStatus).setMessage(message));
+        return result(null,null,new R(resultStatus,message,null));
     }
 
     public static ResponseEntity<R> ok(Object responseBody) {
-        return ResponseEntity.ok(new R().setData(responseBody));
+        return ResponseEntity.ok(new R(null,null,responseBody));
     }
     public static ResponseEntity<R> ok(HttpStatus status, HttpHeaders httpHeaders,Object responseBody) {
-        return result(status,httpHeaders,new R().setData(responseBody));
+        return result(status,httpHeaders,new R(null,null,responseBody));
     }
     public static ResponseEntity<R> ok(HttpStatus status, Object responseBody) {
-        return result(status,null,new R().setData(responseBody));
+        return result(status,null,new R(null,null,responseBody));
     }
 
     private static ResponseEntity<R> result(HttpStatus status,HttpHeaders httpHeaders,R responseBody) {
@@ -43,27 +43,27 @@ public class Response {
         return ResponseEntity.status(status).headers(httpHeaders).body(responseBody);
     }
     public static ResponseEntity<R> fail(String message) {
-        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R().setMessage(message));
+        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R(null,message,null));
     }
     public static ResponseEntity<R> fail(ResultStatus resultStatus,String message) {
-        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R().setStatus(resultStatus).setMessage(message));
+        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R(resultStatus,message,null));
     }
     public static ResponseEntity<R> fail(HttpStatus status, String message, HttpHeaders httpHeaders,R responseBody) {
         return result(status,httpHeaders,responseBody);
     }
     public static ResponseEntity<R> fail(HttpStatus httpStatus, ResultStatus resultStatus,String message) {
-        return result(httpStatus,PROBLEM_HEADERS,new R().setMessage(message).setStatus(resultStatus));
+        return result(httpStatus,PROBLEM_HEADERS,new R(resultStatus,message,null));
     }
     public static ResponseEntity<R> fail(HttpStatus httpStatus, ResultStatus resultStatus) {
-        return result(httpStatus,PROBLEM_HEADERS,new R().setStatus(resultStatus));
+        return result(httpStatus,PROBLEM_HEADERS,new R(resultStatus,null,null));
     }
     public static ResponseEntity<R> fail(HttpStatus status, String message) {
-        return result(status,PROBLEM_HEADERS,new R().setMessage(message).setStatus(ResultStatus.SYSTEM_ERROR));
+        return result(status,PROBLEM_HEADERS,new R(ResultStatus.SYSTEM_ERROR,message,null));
     }
     public static ResponseEntity<R> fail(Object responseBody) {
-        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R().setData(responseBody));
+        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R(ResultStatus.SYSTEM_ERROR,null,responseBody));
     }
     public static ResponseEntity<R> fail(ResultStatus resultStatus) {
-        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R().setStatus(resultStatus));
+        return result(HttpStatus.SERVICE_UNAVAILABLE,PROBLEM_HEADERS,new R(resultStatus,null,null));
     }
 }
